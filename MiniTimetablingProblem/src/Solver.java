952ec7ee68458fa9e20008277d75f6b4e03af131
will <will@MCLilP>
2020-11-28 15:54:38 +0000

    Working version 2, checks dobule booked and 5 days or less

---
+++ MiniTimetablingProblem/src/Solver.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class Solver {
+
+	public Solver() {
+
+	}
+
+	public boolean employeesDoubleBooked(Timetable timetable) {
+		Employee[][] table = timetable.getTable();
+		for (int i = 0; i < table[0].length; i++) {
+			Map<Employee, Integer> employeeCount = new HashMap<>();
+			for (int j = 0; j < table.length; j++) {
+				if (employeeCount.containsKey(table[j][i])) {
+					timetable.editEmployeeInTable(getFreeEmployee(timetable, i), j, i);
+					return true;
+				} else {
+					employeeCount.put(table[j][i], 1);
+				}
+			}
+		}
+		return false;
+	}
+
+	private Employee getFreeEmployee(Timetable timetable, int day) {
+		Employee ret = null;
+		Employee[][] table = timetable.getTable();
+		List<Employee> employees = timetable.getEmployees();
+		Collections.shuffle(employees);
+		for (Employee e : employees) {
+			boolean freeOnDay = true;
+			for (int i = 0; i < table.length; i++) {
+				if (e.equals(table[i][day])) {
+					freeOnDay = false;
+				}
+			}
+			if (freeOnDay)
+				return e;
+		}
+		return ret;
+	}
+
+	public boolean employeesWorkingTooMuch(Timetable timetable) {
+		Employee[][] table = timetable.getTable();
+		Map<Employee, Integer> employeeCount = new HashMap<>();
+		for (int i = 0; i < table.length; i++) {
+			for (int j = 0; j < table[i].length; j++) {
+				Employee e = table[i][j];
+				if (employeeCount.containsKey(e) && employeeCount.get(e) >= 5) {
+					timetable.editEmployeeInTable(getFreeEmployee(timetable, j), i, j);
+					return true;
+				} else if (employeeCount.containsKey(e)) {
+					employeeCount.put(e, employeeCount.get(e) + 1);
+				} else {
+					employeeCount.put(e, 1);
+				}
+			}
+		}
+		return false;
+	}
+
+}
---
+++ MiniTimetablingProblem/src/Timetable.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Timetable {
+
+	private int employeesNeeded, daysInTable;
+	private Employee[][] table;
+	private List<Employee> employees = new ArrayList<>();
+	
+	public Timetable(int employeesNeeded, int daysInTable) {
+		this.employeesNeeded = employeesNeeded;
+		this.daysInTable = daysInTable;
+		table = new Employee[employeesNeeded][daysInTable];
+	}
+	
+	public void editEmployeeInTable(Employee newEmployee, int employee, int day) {
+		table[employee][day] = newEmployee;
+	}
+	
+	public Employee[][] getTable(){
+		return table;
+	}
+	
+	public void addEmployeeToList(Employee e) {
+		employees.add(e);
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+}
